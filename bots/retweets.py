4edbf17a3096af8c6150f1297423bb5382844d3f
Obinna Ogbonna <oogbonna@gidimobile.com>
2020-10-13 05:23:09 +0100

    modify the tweet filters

--- bots/retweets.py
+++ bots/retweets.py
@@ -5,6 +5,7 @@ import tweepy
 import logging
 from config import create_api
 import json
+import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -16,18 +17,20 @@ class RetweetListener(tweepy.StreamListener):
         self.me = api.me()
 
     def on_status(self, tweet):
-        logger.info(f"***** Processing tweet id ******* {tweet.id}")
-        if tweet.in_reply_to_status_id is not None or \
-                tweet.user.id == self.me.id:
-            # This tweet is a reply or I'm its author so, ignore it
-            return
-        if not tweet.retweeted:
-            # Retweet, since we have not retweeted it yet
-            try:
-                tweet.retweet()
-            except Exception as e:
-                logger.error(
-                    "********Error on retweet {e} *********", exc_info=True)
+        while True:
+            logger.info(f"***** Processing tweet id ******* {tweet.id}")
+            if tweet.in_reply_to_status_id is not None or \
+                    tweet.user.id == self.me.id:
+                # This tweet is a reply or I'm its author so, ignore it
+                return
+            if not tweet.retweeted:
+                # Retweet, since we have not retweeted it yet
+                try:
+                    tweet.retweet()
+                except:
+                    logger.error(
+                        "********Error on retweet *********", exc_info=True)
+            time.sleep(1800)
 
     def on_error(self, status):
         logger.error(status)
@@ -41,4 +44,4 @@ def main(keywords):
 
 
 if __name__ == "__main__":
-    main(["#SARSMUSTEND", "#ENDSARSNOW"])
+    main(["#SARSMUSTEND", "#ENDSARSNOW, -kill, -destroy, -loot, -boobs, -pussy, -sex"])
